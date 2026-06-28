91aaa642232048fbb4d144dba17c3fe8fb9d03ef
Victorgabrieldsr <Victorgabrieldsr@gmail.com>
2026-06-27 23:24:25 -0300

    rotas da API configuradas e testes validados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,11 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
     public function register(Request $request)
     {
         $request->validate([
