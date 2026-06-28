f6ca89824d20b9badd179254bb041ff9eb7b67a4
Victorgabrieldsr <Victorgabrieldsr@gmail.com>
2026-06-28 00:08:37 -0300

    correção do retorno JSON 'Call to undefined method'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,10 +9,6 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    }
 
     public function register(Request $request)
     {
